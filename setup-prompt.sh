4f254efb0b38117c490c5dabd1e901045b545be7
Peter Galantha <peterg@yahoo-inc.com>
2017-07-19 16:13:31 -0700

    initial version

---
+++ setup-prompt.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget -O ~/.git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+wget -O ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+
+
