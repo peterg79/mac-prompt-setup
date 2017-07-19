2ac0855688b0671d7486f1087f20b00845b58e49
peterg79 <peter.galantha@gmail.com>
2017-07-19 16:16:08 -0700

    Update setup-prompt.sh

--- setup-prompt.sh
+++ setup-prompt.sh
@@ -2,5 +2,7 @@
 
 wget -O ~/.git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 wget -O ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+wget -O ~/.mac-prompt.bash https://raw.githubusercontent.com/peterg79/mac-prompt-setup/master/profile
 
+echo "source ~/.mac-prompt.bash" >> ~/.bash_profile
 
